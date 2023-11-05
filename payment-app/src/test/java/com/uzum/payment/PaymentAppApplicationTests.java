c6559c26dca7e2849155209cca770b63aa93394d
nurmukhammadsunnatullayev <n.sunatullaev@uzum.com>
2023-11-05 15:52:24 +0500

    Initialized project

---
+++ payment-app/src/test/java/com/uzum/payment/PaymentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uzum.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
